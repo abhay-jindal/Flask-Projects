34749fd1ccaf156c34307057c115cc12385bf76c
abhay-jindal <jindal1808@gmail.com>
2021-04-28 21:44:18 +0530

    static folder added

---
+++ flaskblog/static/account.js
@@ -0,0 +1,89 @@
+
+function checkEmailValidity() {
+    email = $('#email').val();
+    let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email)) {
+        $('#email').removeClass("is-invalid");
+    } else {
+        $('#email').addClass("is-invalid");
+        $('#email-feedback').addClass("invalid-feedback").text('Invalid email address.');
+    }
+    if ($('.is-invalid').length == 0) {
+        $(".btn").prop('disabled', false);
+    } else {
+        $(".btn").prop('disabled', true);
+    }
+}
+
+$('#picture').on('change', function (event) {
+    const file = this.files[0]
+    if (file) {
+        if (['image/png', 'image/jpg', "image/jpeg", "image/gif"].includes(file.type)) {
+            $("#picture-error").text("");
+            $('#picture').removeClass("is-invalid");
+            const reader = new FileReader();
+            reader.onload = function () {
+                $('.account-img').attr('src', this.result);
+            }
+            reader.readAsDataURL(file); // convert to base64 string
+        } else {
+            $("#picture-error").text("File does not have an approved extension: png, jpg, jpeg or gif.");
+            $('#picture').addClass("is-invalid");
+        }
+    }
+
+    $("#picture").change(function () {
+        readURL(this);
+    });
+
+    if ($('.is-invalid').length == 0) {
+        $(".btn").prop('disabled', false);
+    } else {
+        $(".btn").prop('disabled', true);
+    }
+});
+
+
+$(document).ready(function () {
+
+    $('.image-text').on('click', function (e) {
+        $('#picture').click();
+    });
+
+    $('.close').on('click', function (e) {
+        $('.alert').addClass('collapse');
+    });
+
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        var email = $('#email').val();
+        var data = new FormData(this);
+        $("#nav-user, .account-heading").text($('#username').val());
+
+        data.append("current_email", $(".text-secondary").text());
+        ajaxCall(data, "/V1/update/account");
+       
+        function ajaxCall(ajaxData, url) {
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: ajaxData,
+                contentType: false,
+                processData: false,
+            }).done(function (data) {
+                if (data.response) {
+                    $(".alert").removeClass("collapse");
+                    $(".text-secondary").text(email);
+                    if (data.image) {
+                        $(".account-img").attr("src", data.image);
+                        $("#picture").val("");
+                    }
+                } else {
+                    $('#email').addClass("is-invalid");
+                    $('#email-feedback').addClass("invalid-feedback").text(data.error);
+                    $(".btn").prop('disabled', true);
+                }
+            });
+        }
+    });
+});
---
+++ flaskblog/static/login.js
@@ -0,0 +1,75 @@
+
+function checkEmailValidity() {
+    email = $('#email').val();
+    let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email)) {
+        $('#email').removeClass("is-invalid");
+    } else {
+        $('#email').addClass("is-invalid");
+        $('#email-feedback').addClass("invalid-feedback").text('Invalid email address.');
+    }
+
+    if ($('.is-invalid').length == 0) {
+        $(".btn").prop('disabled', false);
+    } else {
+        $(".btn").prop('disabled', true);
+    }
+}
+
+function checkConfirmPassword() {
+    if ($("#password").val() != $("#confirm_password").val()) {
+        $('#confirm_password').addClass("is-invalid");
+        $('#password-feedback').addClass("invalid-feedback").text('Field must be equal to password.');
+    } else {
+        $('#confirm_password').removeClass("is-invalid");
+    }
+
+    if ($('.is-invalid').length == 0) {
+        $(".btn").prop('disabled', false);
+    } else {
+        $(".btn").prop('disabled', true);
+    }
+    
+}
+
+$(document).ready(function () {
+
+    $('.close').on('click', function (e) {
+        $('.alert').addClass('collapse');
+    });
+
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        
+        var title = document.title;
+        var data = new FormData(this);
+        switch (title) {
+            case "Flask Blog - Login":  
+                ajaxCall(data, "/V1/validate/account");
+                break;
+            case "Flask Blog - Register":     
+                ajaxCall(data, "/V1/register/account"); 
+                break;
+            }
+
+        function ajaxCall(data, url) {
+            $.ajax({
+                data: data,
+                type: 'POST',
+                url: url,
+                processData: false,
+                contentType: false
+            }).done(function (data) {
+                if (data.response) {
+                    window.location.href = data.redirect;
+                } else if (data.error) {
+                    $('#email').addClass("is-invalid");
+                    $('#email-feedback').addClass("invalid-feedback").text(data.error);
+                    $(".btn").prop('disabled', true);
+                } else {
+                    $(".alert").removeClass("collapse");
+                }
+            });
+        }
+    });
+});
